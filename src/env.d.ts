8b5824189ccd27dc2996726e6b5b9d40294ee6f6
Adrian Webb <adrian@webb.sh>
2025-05-22 07:39:24 -0400

    Adding an App namespace that defines local variables for user and sessions.

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="vite/client" />
 /// <reference types="../config/types.d.ts" />
+
+declare namespace App {
+  // Note: 'import {} from ""' syntax does not work in .d.ts files.
+  interface Locals {
+    user: import('better-auth').User | null;
+    session: import('better-auth').Session | null;
+  }
+}
