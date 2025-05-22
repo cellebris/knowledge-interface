07619e65a26fcfee57a3209e7f91956edcf5800a
Adrian Webb <adrian@webb.sh>
2025-05-22 07:51:27 -0400

    Adding an initial convertImageToBase64 function in the images utility library.

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -98,3 +98,12 @@ export const adaptOpenGraphImages = async (
 
   return { ...openGraph, ...(adaptedImages ? { images: adaptedImages } : {}) };
 };
+
+export async function convertImageToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result as string);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
